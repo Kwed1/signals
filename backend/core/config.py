b96f76944cc25fb56f41764b6b0be8d31683a148
lcky <lcky>
2024-11-19 01:11:48 +0100

    added docker

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,7 +1,6 @@
 from fastapi.security import OAuth2PasswordBearer
 
 DATABASE_URL = "postgresql+asyncpg://postgres@95.169.201.222:5433/postgres"
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/postgres"
 SECRET_KEY = "key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
