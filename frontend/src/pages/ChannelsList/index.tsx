adba96b5de8270779edf57d3eb10d7fdb771a48e
dimajun <drankendmitrij@gmail.com>
2024-11-25 11:17:09 +0200

    search channels

--- frontend/src/pages/ChannelsList/index.tsx
+++ frontend/src/pages/ChannelsList/index.tsx
@@ -2,58 +2,97 @@ import ChannelCard from 'entities/ChannelCard/ChannelCard';
 import Search from 'entities/Search/Search';
 import ChoiceModal from 'features/ChoiceModal/ChoiceModal';
 import DeleteModal from 'features/DeleteModal/DeleteModal';
-import useModalsStore from 'shared/store/useModalsStore';
-import styles from './ChannelsList.module.scss';
-import Icon from 'shared/ui/Icon/Icon';
-import AdminNav from 'shared/ui/AdminNav/AdminNav';
+import { useEffect, useMemo, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import useChannelsStore from 'shared/store/useChannelsStore';
+import useModalsStore from 'shared/store/useModalsStore';
 import useTokenStore from 'shared/store/useTokenStore';
-import useApi from 'shared/utils/ApiResponseHandler';
 import { Channel } from 'shared/types';
-import { useEffect } from 'react';
+import AdminNav from 'shared/ui/AdminNav/AdminNav';
+import Icon from 'shared/ui/Icon/Icon';
+import useApi from 'shared/utils/ApiResponseHandler';
+import styles from './ChannelsList.module.scss';
 
 export default function ChannelsList() {
+   const [search, setSearch] = useState<string>('');
    const { choiceModalOpen, deleteModalOpen } = useModalsStore();
-   const {channels, setChannels} = useChannelsStore();
-   const {getToken} = useTokenStore();
+   const { channels, setChannels } = useChannelsStore();
+   const { getToken } = useTokenStore();
    let _accessToken = getToken();
    const api = useApi();
 
    const fetchChannels = async () => {
-      const res = await api<Channel[]>({url: '/channel/', method: "GET"});
-      if(res) {
+      const res = await api<Channel[]>({ url: '/channel/', method: 'GET' });
+      if (res) {
          setChannels(res);
       }
-   }
+   };
+
+   const filteredChannels = useMemo(
+      () =>
+         channels.filter(channel =>
+            channel.name.toLowerCase().includes(search.toLowerCase()),
+         ),
+      [channels, search],
+   );
 
    useEffect(() => {
-      if(channels.length > 0) return;
-      if(_accessToken) {
+      if (channels.length > 0) return;
+      if (_accessToken) {
          fetchChannels();
       }
-   }, [_accessToken])
+   }, [_accessToken]);
 
    return (
       <>
          <div className={styles.ChannelsList}>
             <div className={styles.top}>
                <p className={styles.pageHeading}>Channels</p>
-               <NavLink to={'/create-channel'} className={styles.addIcon}>
-                  <Icon size={23} className={styles.icon} id='add-square' color='transparent'/>
+               <NavLink
+                  to={'/create-channel'}
+                  className={styles.addIcon}
+               >
+                  <Icon
+                     size={23}
+                     className={styles.icon}
+                     id='add-square'
+                     color='transparent'
+                  />
                </NavLink>
             </div>
-            <Search />
+            <Search
+               value={search}
+               onChange={e => setSearch(e.target.value)}
+            />
             <div className={styles.list}>
-               {channels.map(channel => (
-                  <ChannelCard key={channel.channel_id} name={channel.name} icon_type={channel.icon_type} admin_id={channel.admin_id} channel_id={channel.channel_id} link={channel.link}/>
-               ))}
-               {channels.length === 0 && <p className={styles.empty}>Channels list empty</p>}
+               {search.length === 0 ?
+                  channels.map(channel => (
+                     <ChannelCard
+                        key={channel.channel_id}
+                        name={channel.name}
+                        icon_type={channel.icon_type}
+                        admin_id={channel.admin_id}
+                        channel_id={channel.channel_id}
+                        link={channel.link}
+                     />
+                  )) : filteredChannels.map(channel => (
+                     <ChannelCard
+                        key={channel.channel_id}
+                        name={channel.name}
+                        icon_type={channel.icon_type}
+                        admin_id={channel.admin_id}
+                        channel_id={channel.channel_id}
+                        link={channel.link}
+                     />
+                  ))}
+               {channels.length === 0 && (
+                  <p className={styles.empty}>Channels list empty</p>
+               )}
             </div>
          </div>
          {choiceModalOpen && <ChoiceModal />}
          {deleteModalOpen && <DeleteModal />}
-         <AdminNav/>
+         <AdminNav />
       </>
    );
 }
