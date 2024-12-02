f9820cdbe440165334f6758f9138c8653e3fe89c
dimajun <drankendmitrij@gmail.com>
2024-12-02 12:39:46 +0200

    fix

--- frontend/src/entities/ChannelCard/ChannelCard.tsx
+++ frontend/src/entities/ChannelCard/ChannelCard.tsx
@@ -26,7 +26,7 @@ export default function ChannelCard({name, admin_id, channel_id, icon_type, link
             <div className={styles.imageWrapper}><div className={styles.image}></div></div>
             <div className={styles.info}>
                 <p className={styles.channelName}>{name}</p>
-                <p className={styles.lastMess}>{last_message?.text}</p>
+                <p className={styles.lastMess}>{last_message && last_message?.text.length > 34 ? `${last_message?.text.slice(0, 35)}...` : last_message?.text}</p>
             </div>
         </div>
     )
